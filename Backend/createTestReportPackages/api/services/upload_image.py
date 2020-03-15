db9b2300f23feac757874c9faee1ba21943a3a1b
aditya.verma <aditya.verma@magicfinserv.com>
2020-03-15 09:23:22 +0530

    structuring of code

--- Backend/createTestReportPackages/api/services/upload_image.py
+++ Backend/createTestReportPackages/api/services/upload_image.py
@@ -1,24 +1,16 @@
 import threading
 from time import time
-
 from flask_restplus import fields
-
 from createTestReportPackages.api import api_response
-from createTestReportPackages.api import custom_fields
-from createTestReportPackages.database.operations.tagging import fetch_primary_key
 from createTestReportPackages.parser import CONFIG
 from createTestReportPackages.utils import platform_exception as exp
 from createTestReportPackages.utils.platform_logging import tagging_logger
-from createTestReportPackages.pipelines import image_operations
+from createTestReportPackages.pipelines import create_docx_from_data
 
-URL = '/ifill/tagger/upload-image'
+URL = '/test-report-generator/create-report'
 
 request_fields = {
-    "image": fields.String('Base64 String of Image', required=True),
-    "templateId": fields.String('Template ID', required=True),
-    "clientId": fields.String('Client ID', required=True),
-    "isEditable": custom_fields.NullableString('Is Editable', required=True)
-}
+    "form_data": fields.String('key and values from the form', required=True)}
 
 
 def func(request_json):
@@ -29,31 +21,22 @@ def func(request_json):
     :return:text response
     """
     start_time = time()
-    template_id = request_json['templateId']
-    client_id = request_json['clientId']
-    image = request_json['image']
-    is_editable = request_json['isEditable']
-    overwrite = int(CONFIG['allowOverwriteTemplate'])
-    if is_editable == "edit":
-        overwrite = 1
+    form_data = request_json['form_data']
     try:
-        tagging_logger.info(f"API START : UPLOAD IMAGE :: TEMPLATE_ID - {template_id} ")
-        client_id = fetch_primary_key.client_id(client_id)
-        image_operations.upload_image_api(image=image, template_id=template_id,
-                                          client_id=client_id, overwrite=overwrite)
-        ocr_thread = threading.Thread(target=image_operations.extract_data_from_ocr,
-                                      args=(image, template_id, client_id))
+        tagging_logger.info(f"API START : UPLOAD IMAGE :: TEMPLATE_ID - ")
+        ocr_thread = threading.Thread(target=create_docx_from_data.create_pdf,
+                                      args=(form_data))
         ocr_thread.start()
         response = api_response.text_response("Successfully uploaded image", api_response.HTTP_SUCCESS_STATUS_CODE)
         end_time = time()
         tagging_logger.info(
-            f"API SUCCESSFUL : UPLOAD IMAGE :: TEMPLATE_ID - {template_id} : Total Time - {end_time - start_time}")
+            f"API SUCCESSFUL : UPLOAD IMAGE :: TEMPLATE_ID -  : Total Time - {end_time - start_time}")
         return response
     except exp.PlatformException as e:
         response = api_response.text_response(e.custom_message, api_response.HTTP_FAILURE_STATUS_CODE)
         end_time = time()
         tagging_logger.exception(
-            f"API FAILED : UPLOAD IMAGE :: TEMPLATE_ID - {template_id} : ERROR - {e.custom_message} : "
+            f"API FAILED : UPLOAD IMAGE :: TEMPLATE_ID -  : ERROR - {e.custom_message} : "
             f"Total Time - {end_time - start_time}")
         return response
 
@@ -61,6 +44,6 @@ def func(request_json):
         response = api_response.text_response(str(e), api_response.HTTP_FAILURE_STATUS_CODE)
         end_time = time()
         tagging_logger.exception(
-            f"API FAILED : UPLOAD IMAGE :: TEMPLATE_ID - {template_id} : ERROR - {str(e)} : "
+            f"API FAILED : UPLOAD IMAGE :: TEMPLATE_ID -  : ERROR - {str(e)} : "
             f"Total Time - {end_time - start_time}")
         return response
---
+++ Backend/createTestReportPackages/pipelines/create_docx_from_data.py
@@ -0,0 +1,77 @@
+from pdf2image import convert_from_path
+import os
+import shutil
+import comtypes.client
+
+wdFormatPDF = 17
+
+dict = {
+    "#@TESTREPORTNO@#": "123456",
+    "#@ISSUEDATE@#": "10-11-2019",
+    "#@URI@#": "765678",
+    "#@MANUFACTURER@#": "NOKIA",
+    "#@IDENTIFICATION@#": "ABCDEFGH",
+    "#@SERIALNO@#": "987654",
+    "#@RECIPTNO@#": "8793247",
+    "#@DATEOFRECIPT@#": "1-2-2020",
+    "#@TESTENGINEERDATED@#": "2-2-2020",
+    "#@HODDATED@#": "2-2-2020",
+    "#@BDHDATED@#": "2-2-2020",
+
+}
+
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
+
+def replace_placeholders(xml_file_path):
+    f = open(xml_file_path, 'r', encoding="UTF-8")
+    xml = f.read()
+    for key in dict.keys():
+        xml = xml.replace(key, dict[key])
+    f = open(xml_file_path, "w", encoding="UTF-8")
+    f.write(xml)
+    f.close()
+
+
+def zip_and_convert_to_docx(dst):
+    shutil.make_archive(dst + "/test_report", 'zip', dst)
+    os.rename(dst + '/test_report.zip', dst + '/test_report.docx')
+
+
+def doc_to_pdf(in_file, out_file):
+    word = comtypes.client.CreateObject('Word.Application')
+    doc = word.Documents.Open(in_file)
+    doc.SaveAs(out_file, FileFormat=wdFormatPDF)
+    doc.Close()
+
+
+def pdf_to_image_pdf(out_file, img_pdf_path):
+    pages = convert_from_path(out_file)
+    pages[0].save(img_pdf_path, save_all=True, append_images=pages[1:])
+
+
+def create_pdf(form_data):
+    src = "D:/tryandtest"
+    dst = "D:/temp/guid1"
+    copytree(src, dst)
+    xml_file_path = dst + r"/word/document.xml"
+
+    path = dst + '/word'
+    for filename in os.listdir(path):
+        if not filename.endswith('.xml'): continue
+        fullname = os.path.join(path, filename)
+        replace_placeholders(fullname)
+    zip_and_convert_to_docx(dst)
+    in_file = dst + '/test_report.docx'
+    out_file = dst + '/test_report.pdf'
+    img_pdf_path = dst + '/test_report_img.pdf'
+    doc_to_pdf(in_file, out_file)
+    pdf_to_image_pdf(out_file, img_pdf_path)
